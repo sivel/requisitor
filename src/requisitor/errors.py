1dd7135a183a022dc2e2c683ccada4a01299af88
Matt Martz <matt@sivel.net>
2022-09-15 13:44:20 -0500

    Fix super call

--- src/requisitor/errors.py
+++ src/requisitor/errors.py
@@ -8,5 +8,5 @@ from .response import Response
 
 class HTTPError(urllib.error.HTTPError, Response):
     def __init__(self, url, code, msg, hdrs, fp, req=None):
-        super(HTTPError, self).__init__(url, code, msg, hdrs, fp)
+        super().__init__(url, code, msg, hdrs, fp)
         Response.__init__(self, fp, req, _error=True)
